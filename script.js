b17b359b1eec5fb0171ef2f0a41e6b0f54e2708e
alexandrovsk <alexanderlim762@gmail.com>
2026-07-10 22:19:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+
+document.getElementById("boot").hidden = true;
+
+document.getElementById("desktop").hidden = false;
+
+
+},4000);
+
+
+
+function openPage(page){
+
+window.location.href = page;
+
+}
